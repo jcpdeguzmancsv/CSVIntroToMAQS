aeddde2f9b2be604929c4c1a797267b5e6560ae6
Juan Carlos De Guzman <942315@cognizant.com>
2022-07-06 17:47:01 +0800

    Add project files.

---
+++ MAQS_Session1/Tests/ExtraVSUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using CognizantSoftvision.Maqs.BaseTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    /// <summary>
+    /// Test1 test class
+    /// </summary>
+    [TestClass]
+    public class ExtraVSUnitTest : BaseTest
+    {
+        /// <summary>
+        /// Sample test
+        /// </summary>
+        [TestMethod]
+        public void SampleTest()
+        {
+            this.Log.LogMessage("Sample Test");
+            Assert.IsTrue(true, "true Is Not TRUE");
+        }
+
+        [TestMethod]
+        public void ExtraFunkyTest()
+        {
+            this.Log.LogMessage("Funky Test - Session 1");
+
+            Assert.IsTrue(true, "true Is Not TRUE");
+        }
+    }
+}
